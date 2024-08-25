6f98eddc3fce681e3a80d5afb1b5f37170b63c60
Dev266 <iladavid897@gmail.com>
2024-08-25 16:13:06 +0200

    Code cleanup, comments

--- main.py
+++ main.py
@@ -8,40 +8,47 @@ from Crypto.Protocol.KDF import PBKDF2
 data = b"babababa"
 passwd = "secret"
 
-def generate_key(password: str, key_size: int = 32) -> bytes:
-    password_bytes = password.encode('utf-8')
-    salt = get_random_bytes(16)
-    key = PBKDF2(password_bytes, salt, dkLen=key_size, count=1)
-    return key, salt
+# encrypts / decrypts the json file
+class Cryptographer:
+    @staticmethod
+    def generate_key(password: str, key_size: int = 32) -> bytes:
+        password_bytes = password.encode('utf-8')
+        salt = get_random_bytes(16)
+        key = PBKDF2(password_bytes, salt, dkLen=key_size, count=1)
+        return key, salt
 
-def encryption(data: bytes, key: bytes):
-    iv = get_random_bytes(AES.block_size)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    padded_data = pad(data, AES.block_size)
-    encrypted_data = cipher.encrypt(padded_data)
-    return iv + encrypted_data
+    @staticmethod
+    def encryption(data: bytes, key: bytes):
+        iv = get_random_bytes(AES.block_size)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        padded_data = pad(data, AES.block_size)
+        encrypted_data = cipher.encrypt(padded_data)
+        return iv + encrypted_data
 
-def decryption(encdata: bytes, key: bytes):
-    iv = encdata[:AES.block_size]
-    encrypted_data = encdata[AES.block_size:]
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    decrypted_padded_data = cipher.decrypt(encrypted_data)
-    unpadded_data = unpad(decrypted_padded_data, AES.block_size)
-    return unpadded_data
+    @staticmethod
+    def decryption(encdata: bytes, key: bytes):
+        iv = encdata[:AES.block_size]
+        encrypted_data = encdata[AES.block_size:]
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        decrypted_padded_data = cipher.decrypt(encrypted_data)
+        unpadded_data = unpad(decrypted_padded_data, AES.block_size)
+        return unpadded_data
 
-key, salt = generate_key(passwd)
-encrypted_data = encryption(data, key)
-print("Encrypted:", encrypted_data)
-decrypted_data = decryption(encrypted_data, key)
-print("Decrypted:", decrypted_data.decode('utf-8'))
+    """ key, salt = generate_key(passwd)
+    encrypted_data = encryption(data, key)
+    print("Encrypted:", encrypted_data)
+    decrypted_data = decryption(encrypted_data, key)
+    print("Decrypted:", decrypted_data.decode('utf-8')) """
 
 
 
-
+# command line interface
 class Argparse:
     def __init__(self) -> None:
         parser = argparse.ArgumentParser(description='A Commandline based password manager.',
-                                         usage='<command> |<args>| |<args>| ...')
+                                         help='<command> |<args>| |<args>| ...', 
+                                         usage=f'Available commands: {[method_name for method_name in dir(object)
+                                                                       if callable(getattr(object, method_name))]}')
         parser.add_argument('command', help='Operation to run.')
 
         args = parser.parse_args(sys.argv[1:2])
@@ -49,23 +56,28 @@ class Argparse:
             print(f'Error: Command "{args.command}" is not defined')
             parser.print_help()
         getattr(self, args.command)()
-    
+        
+    # password id is going to be auto assigned and will not be determined by the user
     @staticmethod
     def add_password() -> None:
-        parser = argparse.ArgumentParser(description='Adds new keys', usage='add_password <password>')
+        parser = argparse.ArgumentParser(description='Adds new keys', 
+                                         help='add_password <password>')
 
         parser.add_argument('password')
         args = parser.parse_args(sys.argv[2:])
     
     @staticmethod
     def remove_password() -> None:
-        parser = argparse.ArgumentParser(description='Adds new keys', usage='remove_password <password-id>')
+        parser = argparse.ArgumentParser(description='Adds new keys', 
+                                         help='remove_password <password-id>')
 
         parser.add_argument('password')
         args = parser.parse_args(sys.argv[2:])
+    # list the passwods inside a table using prettytable
     @staticmethod
     def list_passwords() -> None:
-        parser = argparse.ArgumentParser(description='Adds new keys', usage='list_passwords')
+        parser = argparse.ArgumentParser(description='Adds new keys', 
+                                         help='list_passwords')
 
         parser.add_argument('-s', '--sort')
         args = parser.parse_args(sys.argv[2:])
--- savefilemanager.py
+++ savefilemanager.py
@@ -4,28 +4,27 @@ import os
 class jsonFileManager:
     def __init__(self) -> None:
         self.passwords = []
-    
-    def SavePasswords(self) -> None:
+    # save the passwords inside a json file | strictly uses python dictionaries for saving
+    def save_passwords(self) -> None:
         with open('pswd.json', 'w') as file:
             json.dump(self.passwords, file, indent=4)
-
-    def LoadPasswords(self) -> None:
+    # loads the password from the save file | loads it into a list and as dictionaries
+    def load_passwords(self) -> None:
         try:
             with open('pswd.json', 'r') as file:
                 pass
         except FileNotFoundError:
             self.CreateSaveFile()
 
-        if os.stat(r'pswd.json').st_size == 0: 
-            print('firstime')
+        if os.stat(r'pswd.json').st_size == 0:
+            # only runs if the save file is empty >> this will ask for a new master key
+            return 0
         with open('pswd.json', 'r') as file:
             self.passwords = json.load(file)
+
     @staticmethod
-    def CreateSaveFile() -> None:
+    def create_saveFile() -> None:
         try:
             open('pswd.json', 'x')
         except FileExistsError:
             raise Exception(FileExistsError)
-
-a = jsonFileManager()
-a.LoadPasswords()
